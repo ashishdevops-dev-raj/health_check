6fffa3265401c07e52eae9a07421c6087e22d3ea
Ashish Raj <ashishrajdevops@gmail.com>
2025-06-04 22:42:38 +0530

    Update link_checker.py

--- link_checker.py
+++ link_checker.py
@@ -5,8 +5,26 @@ import smtplib
 from email.mime.text import MIMEText
 
 urls = [
-    "https://swimmersweb.com/",
-    # Add more URLs here as needed
+     "https://swimmersweb.com/",
+    "https://swimmersweb.com/register",
+    "https://swimmersweb.com/login",
+    "https://swimmersweb.com/favorites",
+    "https://swimmersweb.com/profile",
+    "https://swimmersweb.com/privacy-policy",
+    "https://swimmersweb.com/terms-conditions",
+    "https://swimmersweb.com/contact",
+    "https://swimmersweb.com/services",
+    "https://swimmersweb.com/swimmer-search",
+    "https://swimmersweb.com/swimmer-report",
+    "https://swimmersweb.com/team-search",
+    "https://swimmersweb.com/team-member-search",
+    "https://swimmersweb.com/meet-search",
+    "https://swimmersweb.com/meet-detail",
+    "https://swimmersweb.com/motivational-time",
+    "https://swimmersweb.com/swim-compare",
+    "https://swimmersweb.com/tools",
+    "https://swimmersweb.com/upcoming-meet",
+    "https://swimmersweb.com/lap-timer"
 ]
 
 def check_links(urls):
